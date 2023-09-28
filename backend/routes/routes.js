5315efacc5fcfe2d2eeb0c375f5893c8a957e070
IsaiDiaz <jsoza057@gmail.com>
2023-09-27 22:37:25 -0400

    Consumiendo endpoint GET grafos/UsuarioId=1 y POST /grafos

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors'
 
 import { getGrafos, createGrafo, updateGrafo} from '../controllers/grafos.js';
 
 const router = express.Router();
 
+router.use(cors());
 router.get('/grafos/:idUsuario', getGrafos);
 router.post('/grafos', createGrafo);
 router.put('/grafos/:idGrafo', updateGrafo);
