569a39524d7ff4086e9e6e8a612beda300cbc03d
IsaiDiaz <jsoza057@gmail.com>
2023-11-04 17:37:55 -0400

    Estilos agregados a arbol componente y funcionalidad completa terminada

--- frontend/src/components/ArbolComponente.vue
+++ frontend/src/components/ArbolComponente.vue
@@ -1,6 +1,8 @@
 <template>
+  <div class="header">
+    <h1 class="header_title">Arboles</h1>
+  </div>
   <div class="container">
-    <h1>Arboles</h1>
     <div class="graph-container">
       <v-network-graph
         class="graph"
@@ -10,60 +12,98 @@
       />
     </div>
     <div class="buttons">
-      <button @click="showAddNodeModal">Agregar Nodo</button>
-      <button @click="showDeleteNodeModal">Eliminar Nodo</button>
-      <button @click="showConstructTreeModal">Generar a partir de recorridos</button>
-      <button @click="inOrderTraversal">Recorrido InOrder</button>
-      <button @click="preOrderTraversal">Recorrido PreOrder</button>
-      <button @click="postOrderTraversal">Recorrido PostOrder</button>
-      <button @click="reset">Reiniciar</button>
+      <button @click="showAddNodeModal" class="buttons_blue">Agregar Nodo</button>
+      <button @click="showDeleteNodeModal" class="buttons_red">Eliminar Nodo</button>
+      <button @click="showConstructTreeModal" class="buttons_yellow">Generar a partir de recorridos</button>
+      <button @click="inOrderTraversal" class="buttons_yellow">Recorrido InOrder</button>
+      <button @click="preOrderTraversal" class="buttons_yellow">Recorrido PreOrder</button>
+      <button @click="postOrderTraversal" class="buttons_yellow">Recorrido PostOrder</button>
+      <button @click="reset" class="buttons_red">Reiniciar</button>
     </div>
 
     <!-- Modales -->
     <div v-if="isAddNodeModalVisible" class="modal">
       <div class="modal-content">
-        <h3>Agregar Nodo</h3>
-        <input v-model="newNodeValue" type="text" placeholder="Ingrese el valor del nodo" />
-        <button @click="addNode">Agregar</button>
-        <button @click="closeAddNodeModal">Cerrar</button>
+        <div class="modal-content-header">
+          <h3 class = "modal-content-header-title">Agregar Nodo</h3>
+          <span class="close" @click="closeAddNodeModal">&times;</span>
+        </div>
+        <div class="modal-content-body">
+          <p class = "modal-content-body-label">Ingrese el valor del nodo a agregar.</p>
+          <input v-model="newNodeValue" type="text" placeholder="Ingrese el valor del nodo" class="modal-content-body-input" />
+        </div>
+        <div class="modal-content-buttons">
+          <button @click="addNode" class="buttons_blue">Agregar</button>
+          <button @click="closeAddNodeModal" class="buttons_red">Cerrar</button>
+        </div>
       </div>
     </div>
 
     <div v-if="isDeleteNodeModalVisible" class="modal">
       <div class="modal-content">
-        <h3>Eliminar Nodo</h3>
-        <input v-model="nodeToDelete" type="text" placeholder="Ingrese el valor del nodo a eliminar" />
-        <button @click="deleteNode">Eliminar</button>
-        <button @click="closeDeleteNodeModal">Cerrar</button>
+        <div class="modal-content-header">
+          <h3 class = "modal-content-header-title">Eliminar Nodo</h3>
+          <span class="close" @click="closeDeleteNodeModal">&times;</span>
+        </div>
+        <div class ="modal-content-body">
+          <p class = "modal-content-body-label">Ingrese el valor del nodo a eliminar.</p>
+          <input v-model="nodeToDelete" type="text" placeholder="Ingrese el valor del nodo a eliminar" class="modal-content-input"/>
+        </div>
+        <div class="modal-content-buttons">
+          <button @click="deleteNode" class="buttons_blue">Eliminar</button>
+          <button @click="closeDeleteNodeModal" class="buttons_red">Cerrar</button>
+        </div>
       </div>
     </div>
 
     <!-- Modal de Alerta -->
     <div v-if="isAlertModalVisible" class="modal">
       <div class="modal-content">
-        <h3>Alerta!</h3>
-        <p>{{alertMessage}}</p>
-        <button @click="closeAlertModal">Cerrar</button>
+        <div class="modal-content-header modal-content-header_alert">
+          <h3 class = "modal-content-header-title">Alerta!</h3>
+          <span class="close" @click="closeAlertModal">&times;</span>
+        </div>
+        <div class="modal-content-body">
+          <p class="modal-content-alert">{{alertMessage}}</p>
+        </div>
+        <div class="modal-content-buttons">
+          <button @click="closeAlertModal" class="buttons_red">Cerrar</button>
+        </div>
       </div>
     </div>
 
     <!-- Modal de Recorridos -->
     <div v-if="isTraversalModalVisible" class="modal">
       <div class="modal-content">
-        <h3>{{ traversalTitle }}</h3>
-        <p>{{ traversalResult }}</p>
-        <button @click="closeTraversalModal">Cerrar</button>
+        <div class="modal-content-header">
+          <h3 class = "modal-content-header-title">{{ traversalTitle }}</h3>
+          <span class="close" @click="closeTraversalModal">&times;</span>
+        </div>
+        <div class="modal-content-body">
+          <p class="modal-content-body-message">{{ traversalResult }}</p>
+        </div>
+        <div class="modal-content-buttons">
+          <button @click="closeTraversalModal" class="buttons_red">Cerrar</button>
+        </div>
       </div>
     </div>
 
     <!--Modal de Contruccion de Arbol-->
     <div v-if="isConstructTreeModalVisible" class="modal">
       <div class="modal-content">
-        <h3>Construir Arbol</h3>
-        <input v-model="inOrder" type="text" placeholder="Ingrese el recorrido InOrder separado por comas" />
-        <input v-model="preOrder" type="text" placeholder="Ingrese el recorrido PreOrder separado por comas" />
-        <button @click="constructTree">Construir</button>
-        <button @click="closeConstructTreeModal">Cerrar</button>
+        <div class="modal-content-header">
+          <h3 class = "modal-content-header-title">Construir Arbol</h3>
+          <span class="close" @click="closeConstructTreeModal">&times;</span>
+        </div>
+        <div class="modal-content-body">
+          <input v-model="inOrder" type="text" placeholder="Ingrese el recorrido InOrder separado por comas" class="modal-content-body-input"/>
+          <input v-model="preOrder" type="text" placeholder="Ingrese el recorrido PreOrder separado por comas"  class="modal-content-body-input"/>
+          <p class="modal-content-body-message">Los recorridos deben tener la misma cantidad de elementos.</p>
+        </div>
+        <div class="modal-content-buttons">
+          <button @click="constructTree" class="buttons_blue">Construir</button>
+          <button @click="closeConstructTreeModal" class="buttons_red">Cerrar</button>
+        </div>
       </div>
     </div>
 
@@ -178,16 +218,28 @@ function updateGraphData() {
 }
 
 function inOrderTraversal() {
+  if (tree.root === null) {
+    showAlertModal("El árbol está vacío.");
+    return;
+  }
   const result = tree.InOrder();
   showTraversalModal("Recorrido InOrder", result.join(" -> "));
 }
 
 function preOrderTraversal() {
+  if (tree.root === null) {
+    showAlertModal("El árbol está vacío.");
+    return;
+  }
   const result = tree.PreOrder();
   showTraversalModal("Recorrido PreOrder", result.join(" -> "));
 }
 
 function postOrderTraversal() {
+  if (tree.root === null) {
+    showAlertModal("El árbol está vacío.");
+    return;
+  }
   const result = tree.PostOrder();
   showTraversalModal("Recorrido PostOrder", result.join(" -> "));
 }
@@ -214,23 +266,36 @@ function reset() {
 </script>
 
 <style>
+
 .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  margin-top: 20px;
   height: 100vh;
 }
 
 .graph-container {
   width: 100%;
   height: 70vh;
-  border: 1px solid #000;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
 }
 
 .graph {
   width: 100%;
   height: 100%;
+}
+
+.header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+  background-color: #2196f3;
+}
+
+.header_title{
+  color: #fff;
+  font-size: 30px;  
+  font-family: sans-serif;
 }
 
 .buttons {
@@ -241,15 +306,51 @@ function reset() {
   margin-top: 20px;
 }
 
+button{
+  border: none;
+  background-color: none;
+}
+
 .buttons button {
   padding: 10px 20px;
   font-size: 16px;
+}
+
+.buttons_red {
+  background-color: #f44336;
+  color: #fff;
+  border-radius: 10px;
+}
+
+.buttons_blue {
+  background-color: #2196f3;
+  color: #fff;
+  border-radius: 10px;
+}
+
+.buttons_yellow {
+  background-color: #ffeb3b;
+  color: #000;
+  border-radius: 10px;
+}
+
+.buttons_red:hover {
+  background-color: #d32f2f;
+}
+
+.buttons_blue:hover {
+  background-color: #1976d2;
+}
+
+.buttons_yellow:hover {
+  background-color: #fbc02d;
 }
 
 .modal {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   position: fixed;
   top: 0;
   left: 0;
@@ -260,15 +361,97 @@ function reset() {
 }
 
 .modal-content {
+  width: 400px;
+  height: wrap-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: #fff;
-  padding: 20px;
-  border-radius: 5px;
+  border-radius: 25px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   text-align: center;
 }
 
+.modal-content-header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  border-radius: 25px 25px 0 0;
+  background-color: #2196f3;
+  margin-bottom: 20px;
+}
+
+
+.close{
+  color: #fff;
+  cursor: pointer;
+  padding-right: 10px;
+}
+
+
+.modal-content-header-title{
+  margin-bottom: 20px;
+  padding-left: 10px;
+  font-family: sans-serif;
+  font-size: 20px;
+  color: #fff;
+}
+
+.modal-content-body{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.modal-content-body-label{
+  font-family: sans-serif;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.modal-content-body-input{
+  width: 80%;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  margin-bottom: 20px;
+  padding-left: 10px;
+}
+
+.modal-content-body-input:focus{
+  outline: #2196f3;
+}
+
+.modal-content-body-message{
+  font-family: sans-serif;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.modal-content-alert{
+  font-family: sans-serif;
+  font-size: 16px;
+  margin-bottom: 10px;
+  color: #f44336;
+}
+
+.modal-content-buttons{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
 .modal button {
   margin-top: 10px;
+  margin-right: 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+
 }
 
 </style>
